f2d2e726099c71c181586736b9f0796ba486672d
stern <ds@zettelstore.de>
2024-04-22 15:12:37 +0000

    Update license text to reflect the new package name
    FossilOrigin-Name: ef1822e0ca407051446a1d57dbc7d35500f4df646f658e40c45ea8b742965191

--- sxhtml/printer.go
+++ sxhtml/printer.go
@@ -1,9 +1,9 @@
 //-----------------------------------------------------------------------------
 // Copyright (c) 2023-present Detlef Stern
 //
-// This file is part of sxhtml.
+// This file is part of sxwebs.
 //
-// sxhtml is licensed under the latest version of the EUPL // (European Union
+// sxwebs is licensed under the latest version of the EUPL // (European Union
 // Public License). Please see file LICENSE.txt for your rights and obligations
 // under this license.
 //
