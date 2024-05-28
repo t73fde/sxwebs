506968f67df64598d1a1a1b28b4671f722996cf1
stern <ds@zettelstore.de>
2024-05-28 14:46:53 +0000

    sxforms: add textarea and select fields
    FossilOrigin-Name: a5c2b351e40798c98d8f619a2f4c96fa7338d377ac7d40ee0c00691b20869acb

--- sxforms/fields.go
+++ sxforms/fields.go
@@ -14,6 +14,7 @@
 package sxforms
 
 import (
+	"fmt"
 	"time"
 
 	"t73f.de/r/sx"
@@ -23,7 +24,6 @@ import (
 // Field represents a HTTP form field.
 type Field interface {
 	Name() string
-	Label() string
 	Value() string
 	Clear()
 	SetValue(string) error
@@ -31,8 +31,10 @@ type Field interface {
 	Render(string, []string) sx.Object
 }
 
-// InputField represents a HTTP <input> field.
-type InputField struct {
+// ----- <input ...> fields
+
+// InputElement represents a HTTP <input> field.
+type InputElement struct {
 	itype      string
 	name       string
 	label      string
@@ -49,16 +51,15 @@ const (
 	itypeText     = "text"
 )
 
-func (fd *InputField) Name() string  { return fd.name }
-func (fd *InputField) Label() string { return fd.label }
-func (fd *InputField) Value() string {
+func (fd *InputElement) Name() string { return fd.name }
+func (fd *InputElement) Value() string {
 	if fd.itype == itypeSubmit {
 		return ""
 	}
 	return fd.value
 }
 
-func (fd *InputField) Clear() {
+func (fd *InputElement) Clear() {
 	if fd.itype != itypeSubmit {
 		fd.value = ""
 	}
@@ -69,7 +70,7 @@ const (
 	htmlDateLayout = "2006-01-02"
 )
 
-func (fd *InputField) SetValue(value string) error {
+func (fd *InputElement) SetValue(value string) error {
 	fd.value = value
 	switch fd.itype {
 	case itypeDate:
@@ -80,58 +81,37 @@ func (fd *InputField) SetValue(value string) error {
 	return nil
 }
 
-func (fd *InputField) Validators() []Validator { return fd.validators }
+func (fd *InputElement) Validators() []Validator { return fd.validators }
 
-func (fd *InputField) Render(fieldID string, messages []string) sx.Object {
+func (fd *InputElement) Render(fieldID string, messages []string) sx.Object {
 	var flb sx.ListBuilder
 	flb.Add(sx.MakeSymbol("div"))
-	if label := fd.label; label != "" {
-		flb.Add(sx.MakeList(
-			sx.MakeSymbol("label"),
-			sx.MakeList(
-				sxhtml.SymAttr,
-				sx.Cons(sx.MakeSymbol("for"), sx.MakeString(fieldID)),
-			),
-			sx.MakeString(label),
-		))
+	if label := renderLabel(fieldID, fd.label); label != nil {
+		flb.Add(label)
 	}
+	flb.ExtendBang(renderMessages(messages))
 
-	for _, msg := range messages {
-		flb.Add(sx.MakeList(
-			sx.MakeSymbol("span"),
-			sx.MakeList(
-				sxhtml.SymAttr,
-				sx.Cons(sx.MakeSymbol("class"), sx.MakeString("message")),
-			),
-			sx.MakeString(msg),
-		))
-	}
-
-	var wlb sx.ListBuilder
-	wlb.Add(sxhtml.SymAttr)
-	wlb.Add(sx.Cons(sx.MakeSymbol("id"), sx.MakeString(fieldID)))
-	wlb.Add(sx.Cons(sx.MakeSymbol("name"), sx.MakeString(fd.name)))
-	wlb.Add(sx.Cons(sx.MakeSymbol("type"), sx.MakeString(fd.itype)))
-	wlb.Add(sx.Cons(sx.MakeSymbol("value"), sx.MakeString(fd.value)))
+	var attrLb sx.ListBuilder
+	attrLb.Add(sxhtml.SymAttr)
+	attrLb.Add(sx.Cons(sx.MakeSymbol("id"), sx.MakeString(fieldID)))
+	attrLb.Add(sx.Cons(sx.MakeSymbol("name"), sx.MakeString(fd.name)))
+	attrLb.Add(sx.Cons(sx.MakeSymbol("type"), sx.MakeString(fd.itype)))
+	attrLb.Add(sx.Cons(sx.MakeSymbol("value"), sx.MakeString(fd.value)))
 	if fd.autofocus {
-		wlb.Add(sx.Cons(sx.MakeSymbol("autofocus"), sx.Nil()))
+		attrLb.Add(sx.Cons(sx.MakeSymbol("autofocus"), sx.Nil()))
 	}
-	for _, validator := range fd.validators {
-		if valAttrs := validator.Attributes(); valAttrs != nil {
-			wlb.ExtendBang(valAttrs)
-		}
-	}
-	flb.Add(sx.MakeList(sx.MakeSymbol("input"), wlb.List()))
+	attrLb.ExtendBang(renderValidators(fd.validators))
+	flb.Add(sx.MakeList(sx.MakeSymbol("input"), attrLb.List()))
 
 	return flb.List()
 }
 
 // SetAutofocus for the field.
-func (fd *InputField) SetAutofocus() *InputField { fd.autofocus = true; return fd }
+func (fd *InputElement) SetAutofocus() *InputElement { fd.autofocus = true; return fd }
 
 // TextField builds a new text field.
-func TextField(name, label string, validators ...Validator) *InputField {
-	return &InputField{
+func TextField(name, label string, validators ...Validator) *InputElement {
+	return &InputElement{
 		itype:      itypeText,
 		name:       name,
 		label:      label,
@@ -140,8 +120,8 @@ func TextField(name, label string, validators ...Validator) *InputField {
 }
 
 // DateField builds a new field to enter dates.
-func DateField(name, label string, validators ...Validator) *InputField {
-	return &InputField{
+func DateField(name, label string, validators ...Validator) *InputElement {
+	return &InputElement{
 		itype:      itypeDate,
 		name:       name,
 		label:      label,
@@ -150,8 +130,8 @@ func DateField(name, label string, validators ...Validator) *InputField {
 }
 
 // PasswordField builds a new password field.
-func PasswordField(name, label string, validators ...Validator) *InputField {
-	return &InputField{
+func PasswordField(name, label string, validators ...Validator) *InputElement {
+	return &InputElement{
 		itype:      itypePassword,
 		name:       name,
 		label:      label,
@@ -160,10 +140,168 @@ func PasswordField(name, label string, validators ...Validator) *InputField {
 }
 
 // SubmitField builds a new submit field.
-func SubmitField(name, value string) *InputField {
-	return &InputField{
+func SubmitField(name, value string) *InputElement {
+	return &InputElement{
 		itype: itypeSubmit,
 		name:  name,
 		value: value,
 	}
+}
+
+// ----- <textarea ...>...</textarea> field
+
+// TextAreaElement represents the corresponding textarea form element.
+type TextAreaElement struct {
+	name       string
+	label      string
+	rows       uint32
+	cols       uint32
+	value      string
+	validators []Validator
+}
+
+// TextAreaField creates a new text area element.
+func TextAreaField(name, label string, validators ...Validator) *TextAreaElement {
+	return &TextAreaElement{
+		name:       name,
+		label:      label,
+		validators: validators,
+	}
+}
+func (tae *TextAreaElement) Name() string  { return tae.name }
+func (tae *TextAreaElement) Value() string { return tae.value }
+func (tae *TextAreaElement) Clear()        { tae.value = "" }
+func (tae *TextAreaElement) SetValue(value string) error {
+	tae.value = value
+	return nil
+}
+func (tae *TextAreaElement) Validators() []Validator { return tae.validators }
+func (tae *TextAreaElement) Render(fieldID string, messages []string) sx.Object {
+	var flb sx.ListBuilder
+	flb.Add(sx.MakeSymbol("div"))
+	if label := renderLabel(fieldID, tae.label); label != nil {
+		flb.Add(label)
+	}
+	flb.ExtendBang(renderMessages(messages))
+	var attrLb sx.ListBuilder
+	attrLb.Add(sxhtml.SymAttr)
+	attrLb.Add(sx.Cons(sx.MakeSymbol("id"), sx.MakeString(fieldID)))
+	attrLb.Add(sx.Cons(sx.MakeSymbol("name"), sx.MakeString(tae.name)))
+	if rows := tae.rows; rows > 0 {
+		attrLb.Add(sx.Cons(sx.MakeSymbol("rows"), sx.MakeString(fmt.Sprint(rows))))
+	}
+	if cols := tae.cols; cols > 0 {
+		attrLb.Add(sx.Cons(sx.MakeSymbol("cols"), sx.MakeString(fmt.Sprint(cols))))
+	}
+	attrLb.ExtendBang(renderValidators(tae.validators))
+
+	flb.Add(sx.MakeList(sx.MakeSymbol("textarea"), attrLb.List(), sx.MakeString(tae.value)))
+	return flb.List()
+}
+
+// ----- <select ...>...</select> field
+
+// SelectElement represents the corresponding select form element.
+type SelectElement struct {
+	name       string
+	label      string
+	choices    []string
+	value      string
+	validators []Validator
+}
+
+// TextAreaField creates a new text area element.
+func SelectField(name, label string, choices []string, validators ...Validator) *SelectElement {
+	if len(choices)%2 != 0 {
+		panic(fmt.Sprintf("choices must have even number of values: %v", choices))
+	}
+	return &SelectElement{
+		name:       name,
+		label:      label,
+		choices:    choices,
+		validators: validators,
+	}
+}
+func (se *SelectElement) Name() string  { return se.name }
+func (se *SelectElement) Value() string { return se.value }
+func (se *SelectElement) Clear()        { se.value = "" }
+func (se *SelectElement) SetValue(value string) error {
+	se.value = value
+	for i := 0; i < len(se.choices); i += 2 {
+		if se.choices[i] == value {
+			return nil
+		}
+	}
+	return fmt.Errorf("no such choice: %q", value)
+}
+func (se *SelectElement) Validators() []Validator { return se.validators }
+func (se *SelectElement) Render(fieldID string, messages []string) sx.Object {
+	var flb sx.ListBuilder
+	flb.Add(sx.MakeSymbol("div"))
+	if label := renderLabel(fieldID, se.label); label != nil {
+		flb.Add(label)
+	}
+	flb.ExtendBang(renderMessages(messages))
+	var attrLb sx.ListBuilder
+	attrLb.Add(sxhtml.SymAttr)
+	attrLb.Add(sx.Cons(sx.MakeSymbol("id"), sx.MakeString(fieldID)))
+	attrLb.Add(sx.Cons(sx.MakeSymbol("name"), sx.MakeString(se.name)))
+	attrLb.ExtendBang(renderValidators(se.validators))
+
+	var wlb sx.ListBuilder
+	wlb.Add(sx.MakeSymbol("select"))
+	wlb.Add(attrLb.List())
+	for i := 0; i < len(se.choices); i += 2 {
+		choice := se.choices[i]
+		text := se.choices[i+1]
+		var alb sx.ListBuilder
+		alb.Add(sxhtml.SymAttr)
+		alb.Add(sx.Cons(sx.MakeSymbol("value"), sx.MakeString(choice)))
+		if se.value == choice {
+			alb.Add(sx.Cons(sx.MakeSymbol("selected"), sx.Nil()))
+		}
+		wlb.Add(sx.MakeList(sx.MakeSymbol("option"), alb.List(), sx.MakeString(text)))
+	}
+
+	flb.Add(wlb.List())
+	return flb.List()
+}
+
+// ----- General utility functions for rendering etc.
+
+func renderLabel(fieldID, label string) *sx.Pair {
+	if label == "" {
+		return nil
+	}
+	return sx.MakeList(
+		sx.MakeSymbol("label"),
+		sx.MakeList(
+			sxhtml.SymAttr,
+			sx.Cons(sx.MakeSymbol("for"), sx.MakeString(fieldID)),
+		),
+		sx.MakeString(label),
+	)
+}
+
+func renderMessages(messages []string) *sx.Pair {
+	var lb sx.ListBuilder
+	for _, msg := range messages {
+		lb.Add(sx.MakeList(
+			sx.MakeSymbol("span"),
+			sx.MakeList(
+				sxhtml.SymAttr,
+				sx.Cons(sx.MakeSymbol("class"), sx.MakeString("message")),
+			),
+			sx.MakeString(msg),
+		))
+	}
+	return lb.List()
+}
+
+func renderValidators(validators []Validator) *sx.Pair {
+	var lb sx.ListBuilder
+	for _, validator := range validators {
+		lb.ExtendBang(validator.Attributes())
+	}
+	return lb.List()
 }
